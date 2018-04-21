f5ba181240829109bda42e3ac622dee2d060c00c
Arthur Truong <arthurtruong.work@gmail.com>
2018-04-22 00:28:11 +1000

    Initial commit

---
+++ js/animation.js
@@ -0,0 +1,17 @@
+// (function() {
+//
+//   var quotes = $(".quotes");
+//   var quoteIndex = -1;
+//
+//   function showNextQuote() {
+//       ++quoteIndex;
+//       quotes.eq(quoteIndex % quotes.length)
+//           $(".div1").fadeIn(1500)
+//           $(".div2").delay(1500)
+//           $(".div3").fadeOut(1500, showNextQuote);
+//
+//   }
+//
+//   showNextQuote();
+//
+// })();
